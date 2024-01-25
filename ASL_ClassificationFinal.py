93c08a8b241e604247897f29ac159b221649c60a
Basuel24 <92056090+Basuel24@users.noreply.github.com>
2024-01-26 06:49:32 +0800

    Update ASL_ClassificationFinal.py

--- ASL_ClassificationFinal.py
+++ ASL_ClassificationFinal.py
@@ -157,7 +157,7 @@ with tabs[1]:
     count = 0
 ##################################################################
     train_df = pd.read_csv('dataset/train.csv')
-    train_df = np.array(train_df, dtype='float32')
+    #train_df = np.array(train_df, dtype='float32')
     train_df.rename(columns={'label':'Label'},inplace = True)
     train_df = train_df.sample(frac = 1.0).reset_index(drop = True)
 ##################################################################

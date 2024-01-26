7af1c99164b27530cb5f8975fdc002c396f14329
Basuel24 <92056090+Basuel24@users.noreply.github.com>
2024-01-26 18:10:17 +0800

    Update ASL_ClassificationFinal.py

--- ASL_ClassificationFinal.py
+++ ASL_ClassificationFinal.py
@@ -151,18 +151,37 @@ with tabs[1]:
     #end of CNN Baseline Prediction
 ################################################################################################
 with tabs[2]:
-    import streamlit as st
+    import glob
+    import random
+    import base64
     import pandas as pd
-    from PIL import Image
     
-    # Load your dataset
-    #read dataframe from csv
-    df = pd.read_csv('dataset/train.csv')
-    # load the image from binary file, given the path from the Dataframe
-    new_im= np.load(df.iloc[1])
-    # show the loaded image
-    plt.imshow(new_im)
-    plt.show()
+    from PIL import Image
+    from io import BytesIO
+    from IPython.display import HTML
+
+    pd.set_option('display.max_colwidth', -1)
+    
+    def get_thumbnail(path):
+        i = Image.open(path)
+        i.thumbnail((150, 150), Image.LANCZOS)
+        return i
+    
+    def image_base64(im):
+        if isinstance(im, str):
+            im = get_thumbnail(im)
+        with BytesIO() as buffer:
+            im.save(buffer, 'jpeg')
+            return base64.b64encode(buffer.getvalue()).decode()
+    
+    def image_formatter(im):
+        return f'<img src="data:image/jpeg;base64,{image_base64(im)}">'
+
+    dogs = pd.read_csv('dataset/train.csv')
+    dogs = dogs.sample(20)
+    dogs['file'] = dogs.id.map(lambda id: f'../input/train/{id}.jpg')
+    dogs['image'] = dogs.file.map(lambda f: get_thumbnail(f))
+    dogs.head()
 
 ################################################################################################
     #end of CNN Baseline Prediction

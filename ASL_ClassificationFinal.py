eebbc2834aedcce1d8e7e198b35a050c40865454
Basuel24 <92056090+Basuel24@users.noreply.github.com>
2024-01-26 11:56:39 +0800

    Update ASL_ClassificationFinal.py

--- ASL_ClassificationFinal.py
+++ ASL_ClassificationFinal.py
@@ -173,7 +173,7 @@ with tabs[1]:
     #               1   2   3   4   5   6   7   8   9  {J} 10  11  12  13  14  15  16  17  18  19  20  21  22  23  24 {Z}
     class_names = ['A','B','C','D','E','F','G','H','I','','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','']
     a = 0 
-    print(train_df.iloc[i])
+    
     for i, ax in enumerate(axes.flat):        
         inputImage = to_image(train_df.iloc[i])
         img = inputImage
